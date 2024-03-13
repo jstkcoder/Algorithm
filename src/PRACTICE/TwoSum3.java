b9fd668d23a21bdee9f632d8253052e58df098a3
jaeseong <js.tkcoder@gmail.com>
2024-03-13 20:38:22 +0900

    Algorithm_Practice : twosum3 add

---
+++ src/PRACTICE/TwoSum3.java
@@ -0,0 +1,35 @@
+package PRACTICE;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TwoSum3 {
+
+    public static int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement)) {
+                return new int[]{map.get(complement), i};
+            }
+            map.put(nums[i], i);
+        }
+
+        throw new IllegalArgumentException("No two sum solution");
+    }
+
+    public static void main(String[] args) {
+        int[] nums1 = {2, 7, 11, 15};
+        int target1 = 9;
+        int[] nums2 = {3, 2, 4};
+        int target2 = 6;
+
+        int[] result1 = twoSum(nums1, target1);
+        System.out.println("num1의 결과 " + target1 + ": [" + result1[0] + ", " + result1[1] + "]");
+
+        int[] result2 = twoSum(nums2, target2);
+        System.out.println("nums2의 결과 " + target2 + ": [" + result2[0] + ", " + result2[1] + "]");
+    }
+}
+
