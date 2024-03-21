bc2b9a3c0aa1a65728221e0685a0eecf2ac08323
jaeseong <js.tkcoder@gmail.com>
2024-03-21 18:01:11 +0900

    Algorithm_Practice : MaximumMultiple another solution add

--- src/PRACTICE/MaximumMultiple.java
+++ src/PRACTICE/MaximumMultiple.java
@@ -27,3 +27,28 @@ public class MaximumMultiple {
 
     }
 }
+// 또다른 솔루션
+//    public static int maxProduct(int[] nums) {
+//        if (nums == null || nums.length < 2) {
+//            throw new IllegalArgumentException("Input array must contain at least two elements");
+//        }
+//
+//        int maxProduct = Integer.MIN_VALUE;
+//
+//        for (int i = 0; i < nums.length; i++) {
+//            for (int j = i + 1; j < nums.length; j++) {
+//                int product = nums[i] * nums[j];
+//                maxProduct = Math.max(maxProduct, product);
+//            }
+//        }
+//
+//        return maxProduct;
+//    }
+//
+//    public static void main(String[] args) {
+//        int[] nums1 = {1, 2, 3, 4};
+//        int[] nums2 = {-1, -2, -3, -4};
+//
+//        System.out.println("Maximum product of two integers in nums1: " + maxProduct(nums1));
+//        System.out.println("Maximum product of two integers in nums2: " + maxProduct(nums2));
+//    }
