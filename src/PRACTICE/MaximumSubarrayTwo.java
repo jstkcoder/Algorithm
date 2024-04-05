7dd89ea4b15a9685d112a66548ae41a64cf1667d
jaeseong <js.tkcoder@gmail.com>
2024-04-05 23:31:26 +0900

    Algorithm_Practice : MaximumSubarrayTwo add

---
+++ src/PRACTICE/MaximumSubarrayTwo.java
@@ -0,0 +1,20 @@
+package PRACTICE;
+
+public class MaximumSubarrayTwo {
+    public static int maxSubArray(int[] nums) {
+        int maxSum = nums[0];
+        int currentSum = nums[0];
+
+        for (int i = 1; i < nums.length; i++) {
+            currentSum = Math.max(nums[i], currentSum + nums[i]);
+            maxSum = Math.max(maxSum, currentSum);
+        }
+
+        return maxSum;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+        System.out.println("최대 합을 갖는 연속적인 부분 배열의 합: " + maxSubArray(nums));
+    }
+}
