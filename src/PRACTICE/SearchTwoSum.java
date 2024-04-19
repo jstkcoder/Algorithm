36f89f9b035db508478ba467763c7473129a914d
jaeseong <js.tkcoder@gmail.com>
2024-04-19 23:18:20 +0900

    Algorithm_Practice : SearchTwoSum add

---
+++ src/PRACTICE/SearchTwoSum.java
@@ -0,0 +1,32 @@
+package PRACTICE;
+
+import java.util.*;
+
+public class SearchTwoSum {
+    public static int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement)) {
+                return new int[] { map.get(complement), i };
+            }
+            map.put(nums[i], i);
+        }
+        throw new IllegalArgumentException("No two sum solution");
+    }
+
+    public static void main(String[] args) {
+        int[] nums1 = {2, 7, 11, 15};
+        int target1 = 9;
+
+        int[] nums2 = {3, 2, 4};
+        int target2 = 6;
+
+        int[] result1 = twoSum(nums1, target1);
+        int[] result2 = twoSum(nums2, target2);
+
+        System.out.println("Indices of the two numbers that sum up to " + target1 + ": [" + result1[0] + ", " + result1[1] + "]");
+        System.out.println("Indices of the two numbers that sum up to " + target2 + ": [" + result2[0] + ", " + result2[1] + "]");
+    }
+}
+
