7ebec1743f5e4453ff725781516513492039f84a
jaeseong <js.tkcoder@gmail.com>
2024-05-12 00:00:09 +0900

    Algorithm_Practice : BufferReaderTest add

--- src/PRACTICE/BufferReaderTest.java
+++ src/PRACTICE/BufferReaderTest.java
@@ -5,7 +5,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class ConsoleTest {
+public class BufferReaderTest {
 
     public static void main(String[] args) {
         try {
