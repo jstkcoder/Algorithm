f0087a72e31ded23b2708e17cc416c34a1a31c69
jaeseong <js.tkcoder@gmail.com>
2024-05-15 23:58:18 +0900

    Backjun : Q25304 add

---
+++ src/BACKJUN/Q25304.java
@@ -0,0 +1,21 @@
+package BACKJUN;
+
+import java.util.Scanner;
+// https://www.acmicpc.net/problem/25304
+public class Q25304 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int X = sc.nextInt();
+        int N = sc.nextInt();
+        int total = 0;
+
+        for(int i = 0; i < N; i++){
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            total += a * b;
+        }
+        if(total == X) System.out.println("Yes");
+        else System.out.println("No");
+
+    }
+}
